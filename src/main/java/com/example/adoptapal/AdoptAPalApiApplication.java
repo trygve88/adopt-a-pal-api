db99920607661af94fb2d0748dfb2c4c07563731
Trygve Johannessen <trygvejohannessen88@gmail.com>
2022-10-09 19:45:54 +0200

    cors fix

--- src/main/java/com/example/adoptapal/AdoptAPalApiApplication.java
+++ src/main/java/com/example/adoptapal/AdoptAPalApiApplication.java
@@ -1,12 +1,20 @@
 package com.example.adoptapal;
 
+import com.example.adoptapal.util.MyConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @SpringBootApplication
 public class AdoptAPalApiApplication {
 
     public static void main(String[] args) {
+        // CORS
+        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
+        ctx.register(MyConfiguration.class);
+        ctx.refresh();
+        ctx.close();
+
         SpringApplication.run(AdoptAPalApiApplication.class, args);
     }
 
---
+++ src/main/java/com/example/adoptapal/util/MyConfiguration.java
@@ -0,0 +1,25 @@
+package com.example.adoptapal.util;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class MyConfiguration {
+
+    public MyConfiguration() {
+        super();
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+            }
+        };
+    }
+}
