84ec2be5e1b43e1f6568744c881e26b5851fc5c0
Trygve Johannessen <trygvejohannessen88@gmail.com>
2022-10-09 17:59:55 +0200

    first

---
+++ src/main/java/com/example/adoptapal/AdoptAPalApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.adoptapal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdoptAPalApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdoptAPalApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/adoptapal/AdoptAPalApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.adoptapal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdoptAPalApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
