cfea7adc444aeed8fc34b0a83b28e400761598c8
trygve88 <trygvejohannessen88@gmail.com>
2022-10-15 15:43:35 +0200

    removed tricks

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -10,4 +10,4 @@ INSERT INTO dog_skill (dog_id, skill_id) VALUES (1,2)
 INSERT INTO dog_skill (dog_id, skill_id) VALUES (2,1)
 INSERT INTO dog_skill (dog_id, skill_id) VALUES (3,1)
 INSERT INTO dog_skill (dog_id, skill_id) VALUES (3,2)
-INSERT INTO dog_skill (dog_id, skill_id) VALUES (3,3)
+
