3f534a344643dc2d28d5f4227741952da229f99e
trygve88 <trygvejohannessen88@gmail.com>
2022-10-15 15:38:00 +0200

    skill service

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,8 +3,8 @@ INSERT INTO dog (name) VALUES ('fant')
 INSERT INTO dog (name) VALUES ('lassie')
 
 INSERT INTO skill (name) VALUES ('housetrained')
-INSERT INTO skill (name) VALUES ('basic obedience')
-INSERT INTO skill (name) VALUES ('basic tricks')
+INSERT INTO skill (name) VALUES ('basic-obedience')
+INSERT INTO skill (name) VALUES ('basic-tricks')
 
 INSERT INTO dog_skill (dog_id, skill_id) VALUES (1,1)
 INSERT INTO dog_skill (dog_id, skill_id) VALUES (1,2)
