d1763f1fedad6034fa7c771df8316e09885514c6
trygve88 <trygvejohannessen88@gmail.com>
2022-10-15 16:33:37 +0200

    breed

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,10 @@
 INSERT INTO dog (name,breed) VALUES ('spike','rottweiler')
 INSERT INTO dog (name,breed) VALUES ('fant','boxer')
 INSERT INTO dog (name,breed) VALUES ('lassie','tervuren')
+INSERT INTO dog (name,breed) VALUES ('tomy','boxer')
+INSERT INTO dog (name,breed) VALUES ('pat','boxer')
+INSERT INTO dog (name,breed) VALUES ('george','boxer')
+INSERT INTO dog (name,breed) VALUES ('rose','boxer')
 
 INSERT INTO skill (name) VALUES ('housetrained')
 INSERT INTO skill (name) VALUES ('basic-obedience')
@@ -12,4 +16,11 @@ INSERT INTO dog_skill (dog_id, skill_id) VALUES (2,1)
 
 INSERT INTO dog_skill (dog_id, skill_id) VALUES (3,1)
 INSERT INTO dog_skill (dog_id, skill_id) VALUES (3,2)
+
+INSERT INTO dog_skill (dog_id, skill_id) VALUES (4,1)
+
+INSERT INTO dog_skill (dog_id, skill_id) VALUES (5,1)
+
+INSERT INTO dog_skill (dog_id, skill_id) VALUES (6,1)
+
 
