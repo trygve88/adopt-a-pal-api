505f79a2b22e818931377888fb48a8980e60ccef
trygve88 <trygvejohannessen88@gmail.com>
2022-10-16 18:43:30 +0200

    breed

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -15,17 +15,19 @@ INSERT INTO skill (name) VALUES ('housetrained')
 INSERT INTO skill (name) VALUES ('basic-obedience')
 
 INSERT INTO dog_skill (dog_id, skill_id) VALUES (1,1)
-INSERT INTO dog_skill (dog_id, skill_id) VALUES (1,2)
-
 INSERT INTO dog_skill (dog_id, skill_id) VALUES (2,1)
-
 INSERT INTO dog_skill (dog_id, skill_id) VALUES (3,1)
-INSERT INTO dog_skill (dog_id, skill_id) VALUES (3,2)
-
 INSERT INTO dog_skill (dog_id, skill_id) VALUES (4,1)
-
 INSERT INTO dog_skill (dog_id, skill_id) VALUES (5,1)
-
 INSERT INTO dog_skill (dog_id, skill_id) VALUES (6,1)
+INSERT INTO dog_skill (dog_id, skill_id) VALUES (8,1)
+INSERT INTO dog_skill (dog_id, skill_id) VALUES (9,1)
+INSERT INTO dog_skill (dog_id, skill_id) VALUES (10,1)
+INSERT INTO dog_skill (dog_id, skill_id) VALUES (12,1)
 
-
+INSERT INTO dog_skill (dog_id, skill_id) VALUES (1,2)
+INSERT INTO dog_skill (dog_id, skill_id) VALUES (3,2)
+INSERT INTO dog_skill (dog_id, skill_id) VALUES (4,2)
+INSERT INTO dog_skill (dog_id, skill_id) VALUES (6,2)
+INSERT INTO dog_skill (dog_id, skill_id) VALUES (7,2)
+INSERT INTO dog_skill (dog_id, skill_id) VALUES (9,2)
