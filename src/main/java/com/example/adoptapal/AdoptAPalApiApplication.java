2fb199f99d7b67744c195e6426957eba6697a7fb
trygve88 <trygvejohannessen88@gmail.com>
2022-10-19 13:20:41 +0200

    30d

--- src/main/java/com/example/adoptapal/AdoptAPalApiApplication.java
+++ src/main/java/com/example/adoptapal/AdoptAPalApiApplication.java
@@ -1,13 +1,10 @@
 package com.example.adoptapal;
 
-import com.example.adoptapal.config.RsaKeyProperties;
 import com.example.adoptapal.util.MyConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-@EnableConfigurationProperties(RsaKeyProperties.class)
 @SpringBootApplication
 public class AdoptAPalApiApplication {
 
